d3c6ca63a6dc392601290072b46ba312776b4763
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-08-10 13:25:03 +0200

    List of Queries

--- 16 Queries with Solutions.sql
+++ 16 Queries with Solutions.sql
@@ -10,26 +10,23 @@
 --|     List of Queries       |--
 --|---------------------------|--
 
---  1: Retrieve the name and address of all employees who work for the "Research" department
---  2: Retrieve the name of each employee who has a dependent with the same gender as the employee
---  3: Retrieve the names of employees who have no dependents
---  4: Retrieve the names of all employees who are managers and who have at least one dependent
---  5: Retrieve the Social Security numbers of all employees who work on project numbers 1, 2, or 3
---  6: Retrieve the sum of the salaries of all employees, the maximum salary, the minimum salary, and the average salary
---  7: Retrieve the sum of the salaries of all employees of the "Research" department,
---  as well as the maximum salary, the minimum salary, and the average salary in this department
---  8: Retrieve the total number of employees in the company
---  9: Retrieve the total number of employees in the "Research" department
---  10: Retrieve the number of distinct salary values in the database
---  11: Retrieve the names of all employees who have two or more dependents
---  12: Retrieve for each department, the number of employees it has, and their average salary according to their gender
---  13: Retrieve for each project, its number, its name, and the number of employees who work on that project
---  14: Retrieve for each department, its department number, the combined salary in that department, and the number of employees who work in that department
---  15: Retrieve for each project on which more than two employees work,
---  the project number, the project name, and the number of employees who work on the project
---  16: Retrieve for each project, the project number, the project name,
---  and the number of employees from the 'administration' who work on the project
-
+--  1: Retrieve both DOB and address of all employees who goes by the name 'John'
+--  2: Retrieve both DOB and address of all employees who goes by the name 'John B. Smith'
+--  3: Retrieve both name and address of all employees who work for the 'Research' department
+--  4: Retrieve the project number, the controlling department number, and the first name of the department manager for every project located in 'Stafford'
+--  5: Retrieve the employeeís complete name and the complete name(s) of his or her immediate supervisor for each employee
+--  6: Retrieve the salary of every employee 
+--  7: Retrieve all distinct salary values
+--  8: Retrieve all project numbers and project names where 'Jennifer Wallace' is involved in as a worker
+--  9: Retrieve all project numbers and project names where 'Jennifer Wallace' is involved in
+--  10: Retrieve all employees who have a 44 in their ssn
+--  11: Retrieve all employees who were born in the 70s
+--  12: Retrieve the salaries of every employee working on the 'Computerization' project after they are given a 10% salary increase
+--  13: Retrieve the names of employees in the 'Research' department whose salary is lover than 33000 USD
+--  14: Retrieve the names of employees in the 'Research' department whose salary is between 33000 USD and 44000 USD
+--  15: Retrieve a list of employees and the projects they are working on, ordered by department and, 
+--  within each department, ordered alphabetically by last name, then first name
+--  16: Retrieve the names of all employees without supervisors
 
 
 --|--------------------------------|--
