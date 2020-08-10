22af4d0e0db3bf44e228c2ef193bed2332626a33
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-08-10 13:11:36 +0200

    Create 16.sql

---
+++ 16.sql
@@ -0,0 +1,170 @@
+--######################################################################--
+--#                                                                    #--
+--#           SQL Queries for Company Database [10.AUG.2020]           #--
+--#                    Author: Paul Panaitescu                         #--
+--#                                                                    #--
+--######################################################################--
+
+
+--|---------------------------|--
+--|     List of Queries       |--
+--|---------------------------|--
+
+--  1: Retrieve the name and address of all employees who work for the "Research" department
+--  2: Retrieve the name of each employee who has a dependent with the same gender as the employee
+--  3: Retrieve the names of employees who have no dependents
+--  4: Retrieve the names of all employees who are managers and who have at least one dependent
+--  5: Retrieve the Social Security numbers of all employees who work on project numbers 1, 2, or 3
+--  6: Retrieve the sum of the salaries of all employees, the maximum salary, the minimum salary, and the average salary
+--  7: Retrieve the sum of the salaries of all employees of the "Research" department,
+--  as well as the maximum salary, the minimum salary, and the average salary in this department
+--  8: Retrieve the total number of employees in the company
+--  9: Retrieve the total number of employees in the "Research" department
+--  10: Retrieve the number of distinct salary values in the database
+--  11: Retrieve the names of all employees who have two or more dependents
+--  12: Retrieve for each department, the number of employees it has, and their average salary according to their gender
+--  13: Retrieve for each project, its number, its name, and the number of employees who work on that project
+--  14: Retrieve for each department, its department number, the combined salary in that department, and the number of employees who work in that department
+--  15: Retrieve for each project on which more than two employees work,
+--  the project number, the project name, and the number of employees who work on the project
+--  16: Retrieve for each project, the project number, the project name,
+--  and the number of employees from the 'administration' who work on the project
+--  17: Retrive the total number of employees whose salaries exceed $32000 in each department which more than two employees
+--  18: Create a view that contains the info about a project name, number of employees working on it, and total salary of them
+
+
+--|--------------------------------|--
+--|     Solutions for Queries      |--
+--|--------------------------------|--
+
+----------------------------------------------------------------------------------------------------------------
+--  1: Retrieve the name and address of all employees who work for the "Research" department
+----------------------------------------------------------------------------------------------------------------
+--  1.1.
+
+--  1.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  2: Retrieve the name of each employee who has a dependent with the same gender as the employee
+----------------------------------------------------------------------------------------------------------------
+--  2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  3: Retrieve the names of employees who have no dependents
+----------------------------------------------------------------------------------------------------------------
+--  3.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  4: Retrieve the names of all employees who are managers and who have at least one dependent
+----------------------------------------------------------------------------------------------------------------
+--  4.1.
+
+
+--  4.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  5: Retrieve the Social Security numbers of all employees who work on project numbers 1, 2, or 3
+----------------------------------------------------------------------------------------------------------------
+--  5.1.
+
+
+--  5.2.
+
+
+--  5.3.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  6: Retrieve the sum of the salaries of all employees, the maximum salary, the minimum salary, and the average salary
+----------------------------------------------------------------------------------------------------------------
+--  6.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  7: Retrieve the sum of the salaries of all employees of the "Research" department,
+--  as well as the maximum salary, the minimum salary, and the average salary in this department
+----------------------------------------------------------------------------------------------------------------
+-- 7.1.
+
+
+-- 7.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  8: Retrieve the total number of employees in the company
+----------------------------------------------------------------------------------------------------------------
+--  8.1.
+
+
+--  8.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  9: Retrieve the total number of employees in the "Research" department
+----------------------------------------------------------------------------------------------------------------
+--  9.1.
+
+--  9.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  10: Retrieve the number of distinct salary values in the database
+----------------------------------------------------------------------------------------------------------------
+--  10.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  11: Retrieve the names of all employees who have two or more dependents
+----------------------------------------------------------------------------------------------------------------
+--  11.1.
+
+
+--  11.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  12: Retrieve for each department, the number of employees it has, and their average salary according to their gender
+----------------------------------------------------------------------------------------------------------------
+--  12.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  13: Retrieve for each project, its number, its name, and the number of employees who work on that project
+----------------------------------------------------------------------------------------------------------------
+--  13.1.
+
+
+--  13.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  14: Retrieve for each department, its department number, the combined salary in that department, and the number of employees who work in that department
+----------------------------------------------------------------------------------------------------------------
+--  14.1.
+
+
+--  14.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  15: Retrieve for each project on which more than two employees work,
+--the project number, the project name, and the number of employees who work on the project
+----------------------------------------------------------------------------------------------------------------
+--  15.1.
+
+   
+--  15.2.
+
+
+----------------------------------------------------------------------------------------------------------------
+--  16: Retrieve for each project, the project number, the project name,
+--and the number of employees from the 'administration' who work on the project
+----------------------------------------------------------------------------------------------------------------
+--  16.
+
+
+----------------------------------------------------------------------------------------------------------------
